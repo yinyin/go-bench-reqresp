dc283b0e05efefc9698a4a18e53ec4cc90e0e054
Yinyin L <yinyinl@gmail.com>
2021-05-17 02:28:03 +0800

    test without reusing object

--- channelbased_test.go
+++ channelbased_test.go
@@ -19,6 +19,7 @@ func workerTestChannelBasedReqRespPlus1(wg *sync.WaitGroup, ch chan *reqresp.Cha
 
 func TestChannelBasedReqResp_WaitComplete_1pass(t *testing.T) {
 	target := reqresp.NewChannelBasedReqResp(7)
+	defer target.Release()
 	ch := make(chan *reqresp.ChannelBasedReqResp)
 	wg := sync.WaitGroup{}
 	wg.Add(1)
@@ -37,6 +38,7 @@ func TestChannelBasedReqResp_WaitComplete_1pass(t *testing.T) {
 
 func TestChannelBasedReqResp_WaitComplete_90pass(t *testing.T) {
 	target := reqresp.NewChannelBasedReqResp(7)
+	defer target.Release()
 	ch := make(chan *reqresp.ChannelBasedReqResp)
 	wg := sync.WaitGroup{}
 	wg.Add(1)
@@ -52,7 +54,50 @@ func TestChannelBasedReqResp_WaitComplete_90pass(t *testing.T) {
 	wg.Wait()
 }
 
-func implBenchmarkParallelChannelBasedReqResp_WaitComplete(b *testing.B, workerCount int, workBufferSize int) {
+func BenchmarkSeqChannelBasedReqResp_WaitComplete_Reuse(b *testing.B) {
+	wg := sync.WaitGroup{}
+	ch := make(chan *reqresp.ChannelBasedReqResp, 1)
+	wg.Add(1)
+	go workerTestChannelBasedReqRespPlus1(&wg, ch)
+	b.ResetTimer()
+	target := reqresp.NewChannelBasedReqResp(0)
+	reqVal := int32(3)
+	for i := 0; i < b.N; i++ {
+		target.Req = reqVal
+		target.Wait(ch)
+		respVal := <-target.RespCh
+		_ = respVal
+		reqVal++
+	}
+	target.Release()
+	b.StopTimer()
+	close(ch)
+	wg.Wait()
+
+}
+
+func BenchmarkSeqChannelBasedReqResp_WaitComplete_NoReuse(b *testing.B) {
+	wg := sync.WaitGroup{}
+	ch := make(chan *reqresp.ChannelBasedReqResp, 1)
+	wg.Add(1)
+	go workerTestChannelBasedReqRespPlus1(&wg, ch)
+	b.ResetTimer()
+	reqVal := int32(3)
+	for i := 0; i < b.N; i++ {
+		target := reqresp.NewChannelBasedReqResp(reqVal)
+		target.Wait(ch)
+		respVal := <-target.RespCh
+		_ = respVal
+		target.Release()
+		reqVal++
+	}
+	b.StopTimer()
+	close(ch)
+	wg.Wait()
+
+}
+
+func implBenchmarkParallelChannelBasedReqResp_WaitComplete_Reuse(b *testing.B, workerCount int, workBufferSize int) {
 	wg := sync.WaitGroup{}
 	ch := make(chan *reqresp.ChannelBasedReqResp, workBufferSize)
 	for workerIdx := 0; workerIdx < workerCount; workerIdx++ {
@@ -70,28 +115,73 @@ func implBenchmarkParallelChannelBasedReqResp_WaitComplete(b *testing.B, workerC
 			_ = respVal
 			reqVal++
 		}
+		target.Release()
 	})
 	b.StopTimer()
 	close(ch)
 	wg.Wait()
 }
 
-func BenchmarkParallelChannelBasedReqResp_WaitComplete_1_1(b *testing.B) {
-	implBenchmarkParallelChannelBasedReqResp_WaitComplete(b, 1, 1)
+func BenchmarkParallelChannelBasedReqResp_WaitComplete_Reuse_1_1(b *testing.B) {
+	implBenchmarkParallelChannelBasedReqResp_WaitComplete_Reuse(b, 1, 1)
 }
 
-func BenchmarkParallelChannelBasedReqResp_WaitComplete_2_1(b *testing.B) {
-	implBenchmarkParallelChannelBasedReqResp_WaitComplete(b, 2, 1)
+func BenchmarkParallelChannelBasedReqResp_WaitComplete_Reuse_2_1(b *testing.B) {
+	implBenchmarkParallelChannelBasedReqResp_WaitComplete_Reuse(b, 2, 1)
 }
 
-func BenchmarkParallelChannelBasedReqResp_WaitComplete_2_2(b *testing.B) {
-	implBenchmarkParallelChannelBasedReqResp_WaitComplete(b, 2, 2)
+func BenchmarkParallelChannelBasedReqResp_WaitComplete_Reuse_2_2(b *testing.B) {
+	implBenchmarkParallelChannelBasedReqResp_WaitComplete_Reuse(b, 2, 2)
 }
 
-func BenchmarkParallelChannelBasedReqResp_WaitComplete_2_4(b *testing.B) {
-	implBenchmarkParallelChannelBasedReqResp_WaitComplete(b, 2, 4)
+func BenchmarkParallelChannelBasedReqResp_WaitComplete_Reuse_2_4(b *testing.B) {
+	implBenchmarkParallelChannelBasedReqResp_WaitComplete_Reuse(b, 2, 4)
 }
 
-func BenchmarkParallelChannelBasedReqResp_WaitComplete_4_2(b *testing.B) {
-	implBenchmarkParallelChannelBasedReqResp_WaitComplete(b, 4, 2)
+func BenchmarkParallelChannelBasedReqResp_WaitComplete_Reuse_4_2(b *testing.B) {
+	implBenchmarkParallelChannelBasedReqResp_WaitComplete_Reuse(b, 4, 2)
+}
+
+func implBenchmarkParallelChannelBasedReqResp_WaitComplete_NoReuse(b *testing.B, workerCount int, workBufferSize int) {
+	wg := sync.WaitGroup{}
+	ch := make(chan *reqresp.ChannelBasedReqResp, workBufferSize)
+	for workerIdx := 0; workerIdx < workerCount; workerIdx++ {
+		wg.Add(1)
+		go workerTestChannelBasedReqRespPlus1(&wg, ch)
+	}
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		reqVal := int32(3)
+		for pb.Next() {
+			target := reqresp.NewChannelBasedReqResp(reqVal)
+			target.Wait(ch)
+			respVal := <-target.RespCh
+			_ = respVal
+			target.Release()
+			reqVal++
+		}
+	})
+	b.StopTimer()
+	close(ch)
+	wg.Wait()
+}
+
+func BenchmarkParallelChannelBasedReqResp_WaitComplete_NoReuse_1_1(b *testing.B) {
+	implBenchmarkParallelChannelBasedReqResp_WaitComplete_NoReuse(b, 1, 1)
+}
+
+func BenchmarkParallelChannelBasedReqResp_WaitComplete_NoReuse_2_1(b *testing.B) {
+	implBenchmarkParallelChannelBasedReqResp_WaitComplete_NoReuse(b, 2, 1)
+}
+
+func BenchmarkParallelChannelBasedReqResp_WaitComplete_NoReuse_2_2(b *testing.B) {
+	implBenchmarkParallelChannelBasedReqResp_WaitComplete_NoReuse(b, 2, 2)
+}
+
+func BenchmarkParallelChannelBasedReqResp_WaitComplete_NoReuse_2_4(b *testing.B) {
+	implBenchmarkParallelChannelBasedReqResp_WaitComplete_NoReuse(b, 2, 4)
+}
+
+func BenchmarkParallelChannelBasedReqResp_WaitComplete_NoReuse_4_2(b *testing.B) {
+	implBenchmarkParallelChannelBasedReqResp_WaitComplete_NoReuse(b, 4, 2)
 }
--- lockbased_test.go
+++ lockbased_test.go
@@ -59,7 +59,47 @@ func TestLockBasedReqResp_WaitComplete_90pass(t *testing.T) {
 	wg.Wait()
 }
 
-func implBenchmarkParallelLockBasedReqResp_WaitComplete(b *testing.B, workerCount int, workBufferSize int) {
+func BenchmarkSeqLockBasedReqResp_WaitComplete_Reuse(b *testing.B) {
+	wg := sync.WaitGroup{}
+	ch := make(chan *reqresp.LockBasedReqResp, 1)
+	wg.Add(1)
+	go workerTestLockBasedReqRespPlus1(&wg, ch)
+	b.ResetTimer()
+	target := reqresp.LockBasedReqResp{}
+	reqVal := int32(3)
+	for i := 0; i < b.N; i++ {
+		target.Req = reqVal
+		target.Resp = 0
+		target.Wait(ch)
+		_ = target.Resp
+		reqVal++
+	}
+	b.StopTimer()
+	close(ch)
+	wg.Wait()
+}
+
+func BenchmarkSeqLockBasedReqResp_WaitComplete_NoReuse(b *testing.B) {
+	wg := sync.WaitGroup{}
+	ch := make(chan *reqresp.LockBasedReqResp, 1)
+	wg.Add(1)
+	go workerTestLockBasedReqRespPlus1(&wg, ch)
+	b.ResetTimer()
+	reqVal := int32(3)
+	for i := 0; i < b.N; i++ {
+		target := reqresp.LockBasedReqResp{
+			Req: reqVal,
+		}
+		target.Wait(ch)
+		_ = target.Resp
+		reqVal++
+	}
+	b.StopTimer()
+	close(ch)
+	wg.Wait()
+}
+
+func implBenchmarkParallelLockBasedReqResp_WaitComplete_Reuse(b *testing.B, workerCount int, workBufferSize int) {
 	wg := sync.WaitGroup{}
 	ch := make(chan *reqresp.LockBasedReqResp, workBufferSize)
 	for workerIdx := 0; workerIdx < workerCount; workerIdx++ {
@@ -83,22 +123,66 @@ func implBenchmarkParallelLockBasedReqResp_WaitComplete(b *testing.B, workerCoun
 	wg.Wait()
 }
 
-func BenchmarkParallelLockBasedReqResp_WaitComplete_1_1(b *testing.B) {
-	implBenchmarkParallelLockBasedReqResp_WaitComplete(b, 1, 1)
+func BenchmarkParallelLockBasedReqResp_WaitComplete_Reuse_1_1(b *testing.B) {
+	implBenchmarkParallelLockBasedReqResp_WaitComplete_Reuse(b, 1, 1)
 }
 
-func BenchmarkParallelLockBasedReqResp_WaitComplete_2_1(b *testing.B) {
-	implBenchmarkParallelLockBasedReqResp_WaitComplete(b, 2, 1)
+func BenchmarkParallelLockBasedReqResp_WaitComplete_Reuse_2_1(b *testing.B) {
+	implBenchmarkParallelLockBasedReqResp_WaitComplete_Reuse(b, 2, 1)
 }
 
-func BenchmarkParallelLockBasedReqResp_WaitComplete_2_2(b *testing.B) {
-	implBenchmarkParallelLockBasedReqResp_WaitComplete(b, 2, 2)
+func BenchmarkParallelLockBasedReqResp_WaitComplete_Reuse_2_2(b *testing.B) {
+	implBenchmarkParallelLockBasedReqResp_WaitComplete_Reuse(b, 2, 2)
 }
 
-func BenchmarkParallelLockBasedReqResp_WaitComplete_2_4(b *testing.B) {
-	implBenchmarkParallelLockBasedReqResp_WaitComplete(b, 2, 4)
+func BenchmarkParallelLockBasedReqResp_WaitComplete_Reuse_2_4(b *testing.B) {
+	implBenchmarkParallelLockBasedReqResp_WaitComplete_Reuse(b, 2, 4)
 }
 
-func BenchmarkParallelLockBasedReqResp_WaitComplete_4_2(b *testing.B) {
-	implBenchmarkParallelLockBasedReqResp_WaitComplete(b, 4, 2)
+func BenchmarkParallelLockBasedReqResp_WaitComplete_Reuse_4_2(b *testing.B) {
+	implBenchmarkParallelLockBasedReqResp_WaitComplete_Reuse(b, 4, 2)
+}
+
+func implBenchmarkParallelLockBasedReqResp_WaitComplete_NoReuse(b *testing.B, workerCount int, workBufferSize int) {
+	wg := sync.WaitGroup{}
+	ch := make(chan *reqresp.LockBasedReqResp, workBufferSize)
+	for workerIdx := 0; workerIdx < workerCount; workerIdx++ {
+		wg.Add(1)
+		go workerTestLockBasedReqRespPlus1(&wg, ch)
+	}
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		reqVal := int32(3)
+		for pb.Next() {
+			target := reqresp.LockBasedReqResp{
+				Req: reqVal,
+			}
+			target.Wait(ch)
+			_ = target.Resp
+			reqVal++
+		}
+	})
+	b.StopTimer()
+	close(ch)
+	wg.Wait()
+}
+
+func BenchmarkParallelLockBasedReqResp_WaitComplete_NoReuse_1_1(b *testing.B) {
+	implBenchmarkParallelLockBasedReqResp_WaitComplete_NoReuse(b, 1, 1)
+}
+
+func BenchmarkParallelLockBasedReqResp_WaitComplete_NoReuse_2_1(b *testing.B) {
+	implBenchmarkParallelLockBasedReqResp_WaitComplete_NoReuse(b, 2, 1)
+}
+
+func BenchmarkParallelLockBasedReqResp_WaitComplete_NoReuse_2_2(b *testing.B) {
+	implBenchmarkParallelLockBasedReqResp_WaitComplete_NoReuse(b, 2, 2)
+}
+
+func BenchmarkParallelLockBasedReqResp_WaitComplete_NoReuse_2_4(b *testing.B) {
+	implBenchmarkParallelLockBasedReqResp_WaitComplete_NoReuse(b, 2, 4)
+}
+
+func BenchmarkParallelLockBasedReqResp_WaitComplete_NoReuse_4_2(b *testing.B) {
+	implBenchmarkParallelLockBasedReqResp_WaitComplete_NoReuse(b, 4, 2)
 }
